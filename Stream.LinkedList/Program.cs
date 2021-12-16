6bbd6af0cbbc7ab3011145a8adbaeebc4cb79a02
Andrii Podkolzin <podkolzzzin@yandex.ru>
2021-12-16 18:52:13 +0200

    sample

--- Stream.LinkedList/Program.cs
+++ Stream.LinkedList/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var list = new List<int>();
+try
+{
+    while (true)
+    {
+        list.Add(0);
+        if (list.Count % 100_000 == 0)
+            Console.WriteLine(list.Count);
+    }
+}
+finally
+{
+    Console.WriteLine(list.Count);
+}
